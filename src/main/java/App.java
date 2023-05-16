6bd134c9951d9c849fb9aff8e4dd6775192942d8
cenrose <115883202+cenrose@users.noreply.github.com>
2023-05-16 15:57:42 +0500

    Initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,27 +1,28 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 public class App {
-    public static void main(String[] args) throws FileNotFoundException {
-        App.CSVReader();
+    static String fileName = "src/main/resources/listOfCities.csv";
+    static List<City> cities = new ArrayList<>();
+    public static void main(String[] args) throws FileNotFoundException{
+        readCitiesFromFile();
+        //CSVinJson();
+        //SortCitiesByName();
+        //SortCitiesByNameAndDistrict();
+        theBiggestPopulation();
     }
-    private static void CSVReader() throws FileNotFoundException {
-            //открыли csv файл для чтения
-            Scanner scanner = new Scanner(new File("src/main/resources/listOfCities.csv"));
+        /*теперь вместо метода CSVReader() - readCitiesFromFile(),
+        который только читает файл, но ничего не выводит*/
+        private static void readCitiesFromFile() throws FileNotFoundException{
+            Scanner scanner = new Scanner(new File(fileName));
 
-            //создали список с городами
-            List<City> cities = new ArrayList<>();
-
-            //пока в сканнере есть строки, считываем далее
-            while(scanner.hasNextLine()){
+            //пока в файле есть строки, считываем далее
+            while (scanner.hasNextLine()){
                 String line = scanner.nextLine();
-                //разделяем строки на части, используя разделитель
                 String[] cityData = line.split(";");
 
                 //проверка длины массива (он должен быть не менее 6 элементов)
-                if(cityData.length >= 6){
+                if (cityData.length >= 6) {
                     //парсим поля из строки
                     String name = cityData[1];
                     String region = cityData[2];
@@ -34,12 +35,60 @@ public class App {
                     cities.add(city);
                 }
             }
-            //выводим список объектов в формате JSON
-            for(int i = 0; i < cities.size(); i++){
-                System.out.println(cities.get(i).toString());
-                if(i< cities.size() - 1){
-                    System.out.println();
+        }
+    /*сортировка списка городов по имени
+    и вывод в формате JSON*/
+        private static void SortCitiesByName(){
+            Comparator<City> nameComparator = Comparator.comparing(City::getName);
+            cities.sort(nameComparator);
+
+            //выводим отсортированный список объектов в формате JSON
+            for (int i = 0; i < cities.size(); i++){
+                System.out.print(cities.get(i).toString());
+                if (i < cities.size()){
+                    System.out.println(",");
                 }
             }
+        }
+    /*сортировка списка городов по имени,
+    федеральному округу
+    и вывод в формате JSON*/
+    private static void SortCitiesByNameAndDistrict(){
+        Comparator<City> nameComparator = Comparator.comparing(City::getName);
+        Comparator<City> districtComparator = Comparator.comparing(City::getDistrict);
+
+        //снала отсортировали по имени, затем по федеральному округу
+        cities.sort(nameComparator);
+        cities.sort(districtComparator);
+
+        //выводим отсортированный список объектов в формате JSON
+        for (int i = 0; i < cities.size(); i++){
+            System.out.print(cities.get(i).toString());
+            if (i < cities.size() - 1){
+                System.out.println(",");
+            }
+        }
+    }
+    //вывод в формате jSON
+    private static void CSVinJson() {
+        for (int i = 0; i < cities.size(); i++) {
+            System.out.println(cities.get(i).toString());
+            if (i < cities.size() - 1) {
+                System.out.println();
+            }
+        }
+    }
+
+    private static void theBiggestPopulation(){
+        Comparator<City> populationComparator = Comparator.comparing(City::getPopulation);
+        cities.sort(populationComparator.reversed());
+
+        //выводим отсортированный список объектов в формате JSON
+        for (int i = 0; i < cities.size(); i++){
+            System.out.print(cities.get(i).toString());
+            if (i < cities.size() - 1){
+                System.out.println(",");
+            }
+        }
     }
 }
